8d6a34a34c9924590b5584d5de52165fda3eaa2e
Mathes Henrique dos Santos Lima <1995.matheus.lima@gmail.com>
2022-12-25 19:12:48 -0300

    First commit and Hello git

---
+++ 001_Hello_world/001Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    string x ;
+    cout << "What is your name?: " ;
+    cin >> x;
+    cout << "Hello " << x << " welcome to my world! "<< endl;
+    
+    return 0;
+}
