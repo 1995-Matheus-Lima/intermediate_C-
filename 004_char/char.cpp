053cdedf4a2428cb75679e0616a582747ee40e8e
Mathes Henrique dos Santos Lima <1995.matheus.lima@gmail.com>
2022-12-27 23:57:13 -0300

    004_char

---
+++ 004_char/char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char c = 'a';
+    char d = 'b';
+    int soma  = c + d;
+    cout << (int)c << endl;
+    cout << soma << endl;
+    return 0;
+}
